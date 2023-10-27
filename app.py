1cfe3a55b3da5937a82def533ee5903a13cb9c78
clarspau <125636195+clarspau@users.noreply.github.com>
2023-10-26 19:55:07 -0700

    Updated app.py Created edit.html & show.html

--- app.py
+++ app.py
@@ -34,5 +34,56 @@ def users_index():
 @app.route('/users/new', methods=["GET"])
 def users_new_form():
     """Page for a form to create a new user"""
-    
+
     return render_template('users/new.html')
+
+
+@app.route("/users/new", methods=["POST"])
+def users_new():
+    """Page to create a new user based on the form submission."""
+
+    new_user = User(
+        first_name=request.form['first_name'],
+        last_name=request.form['last_name'],
+        image_url=request.form['image_url'] or None)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect("/users")
+
+
+@app.route('/users/<int:user_id>')
+def users_show(user_id):
+    """Page that shows user details for a specific user."""
+
+    user = User.query.get_or_404(user_id)
+    return render_template('users/show.html', user=user)
+
+
+@app.route('/users/<int:user_id>/edit')
+def users_edit(user_id):
+    """PAge that shows a form to edit an existing user's information."""
+
+    user = User.query.get_or_404(user_id)
+    return render_template('users/edit.html', user=user)
+
+
+@app.route('/users/<int:user_id>/edit', methods=["POST"])
+def users_update(user_id):
+    """Page to update an existing user's information based on the form submission."""
+
+    user = User.query.get_or_404(user_id)
+    user.first_name = request.form['first_name']
+    user.last_name = request.form['last_name']
+    user.image_url = request.form['image_url']
+    db.session.commit()
+    return redirect("/users")
+
+
+@app.route('/users/<int:user_id>/delete', methods=["POST"])
+def users_destroy(user_id):
+    """Page to delete an existing user based on form submission."""
+
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return redirect("/users")
