d02add45eae83637dc271f07ad990c70123907f3
clarspau <125636195+clarspau@users.noreply.github.com>
2023-10-26 18:39:03 -0700

    Modified the data type in models.py from Text to String. Added Flask DebugToolbar in app.py and added a home route.

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 """Blogly application."""
 
-from flask import Flask
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'itsasecret'
+
+toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
+
+
+@app.route('/')
+def home():
+     """Homepage"""
+
+     return redirect('/users')
--- models.py
+++ models.py
@@ -5,18 +5,21 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-     """User model"""
+    """User model."""
 
-     __tablename__ = "users"
+    __tablename__ = "users"
 
-     id = db.Column(db.Integer, primary_key=True)
-     first_name = db.Column(db.Text, nullable=False)
-     last_name = db.Column(db.Text, nullable=False)
-     image_url = db.Column(db.Text, nullable=False, default="https://img.freepik.com/premium-vector/accoun-vector-icon-with-long-shadow-white-illustration-isolated-blue-round-background-graphic-web-design_549897-771.jpg")
-
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    image_url = db.Column(
+        db.String(200),
+        nullable=False,
+        default="https://img.freepik.com/premium-vector/accoun-vector-icon-with-long-shadow-white-illustration-isolated-blue-round-background-graphic-web-design_549897-771.jpg"
+    )
 
 def connect_db(app):
-     """Connect this database to the given Flask app."""
-     
-     db.app = app
-     db.init_app(app)
+    """Connect this database to the given Flask app."""
+
+    db.app = app
+    db.init_app(app)
