8929e9dbe34f65721bb4ffe2d5f2e6565a95aeb0
clarspau <125636195+clarspau@users.noreply.github.com>
2023-10-31 22:08:18 -0700

    Modified models.py for the Part 2 of the exercise --create another model for blog posts.

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 """Models for Blogly."""
 
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -11,16 +12,29 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(50), nullable=False)
-    last_name = db.Column(db.String(50), nullable=False)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
     image_url = db.Column(
-        db.String(200),
+        db.Text,
         nullable=False,
         default="https://img.freepik.com/premium-vector/accoun-vector-icon-with-long-shadow-white-illustration-isolated-blue-round-background-graphic-web-design_549897-771.jpg"
     )
 
+
+class Post(db.Model):
+    """Blog posts."""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+
 def connect_db(app):
-    """Connect this database to the given Flask app."""
+    """Include this database in your Flask app by setting it up within your Flask app."""
 
     db.app = app
     db.init_app(app)
